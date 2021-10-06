87b3c8790547810d9ca1a0e0ed908f9bea1e7436
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-06 09:41:57 -0500

    Se agrega la estructura base del proyecto y las dependencias de cada servicio.

---
+++ photo-service/src/test/java/com/pragma/photo/PhotoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ registry-service/src/main/java/com/pragma/registry/RegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}

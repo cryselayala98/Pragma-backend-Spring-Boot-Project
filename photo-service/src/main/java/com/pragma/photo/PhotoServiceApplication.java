2836805515307122e7d79394ef88bc40050fd2f6
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-07 11:25:04 -0500

    correccion en el properties del servicio de fotos

--- photo-service/src/main/java/com/pragma/photo/PhotoServiceApplication.java
+++ photo-service/src/main/java/com/pragma/photo/PhotoServiceApplication.java
@@ -2,10 +2,11 @@ package com.pragma.photo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+//@EnableEurekaClient
 @SpringBootApplication
-@EnableEurekaClient
+@EnableMongoRepositories
 public class PhotoServiceApplication {
 
 	public static void main(String[] args) {
